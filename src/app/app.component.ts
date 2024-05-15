6022ac82d185f44d8aca6ddeb5235b723aa18832
simonayo <ayosimon.dev@gmail.com>
2024-05-15 15:21:16 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'blockchain-review-app';
+
+  show: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.show = true;
+    }, 1500);
+  }
 }
